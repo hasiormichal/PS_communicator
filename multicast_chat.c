961963b7f7d043999616e21af6fd1b85615ef65c
Michał Hasior <michalhasior@student.agh.edu.pl>
2022-01-04 10:18:30 +0100

    m

--- multicast_chat.c
+++ multicast_chat.c
@@ -274,7 +274,7 @@ void send_all(int sendfd, SA *sadest, socklen_t salen){
 	scanf("%s",&nazwa);
 	for ( ; ; ) {
 		fgets(wiadomosc,MAXLINE,stdin);
-		if(strcmp(wiadomosc,"exit")){
+		if(wiadomosc[0] == 'x'){
 			snprintf(line,sizeof(line),"%s.\n>%s",nazwa, wiadomosc);
 			if(sendto(sendfd, line, strlen(line), 0, sadest, salen) < 0 )
 			fprintf(stderr,"sendto() error : %s\n", strerror(errno));
