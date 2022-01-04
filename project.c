e4cf66e1cd7bb1f659d28e1a4ac3faaa803b3145
Michał Hasior <michalhasior@student.agh.edu.pl>
2022-01-04 11:54:55 +0100

    m

--- project.c
+++ project.c
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdio.h>
 
 int main(){
     execl("/home/student/PS_communicator/client.exe" , "/home/student/PS_communicator/client.exe" , 4444, NULL );
+    printf("nie wyszlo\n");
     return 0;
 }
