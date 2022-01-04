e4dd78fbb443bbdff0282a532cc6e45ebc499720
Michał Hasior <michalhasior@student.agh.edu.pl>
2022-01-04 12:22:16 +0100

    m

--- project.c
+++ project.c
@@ -6,6 +6,30 @@
 
 int main(){
     //execl("/home/student/PS_communicator/client.exe" , "client" , 4444, NULL );
-    execlp("/home/student/PS_communicator/client" , "client" ,"4444", (char *)NULL);
-    return 0;
+    int wybor = 0;
+    while (1){
+        printf("Wybierz tryb programu:\n");
+        printf("1) indywidualny czat - klient\n");
+        printf("2) indywidualny czat - serwer\n");
+        printf("3) czat grupowy\n");
+        printf("4) wyjdz\n");
+        scanf("%i",&wybor);
+
+        if(wybor == 1){
+            execlp("/home/student/PS_communicator/client" , "client" ,"4444", (char *)NULL);
+        }
+
+        else if (wybor == 2){
+            execlp("/home/student/PS_communicator/server" , "server" ,"4444", (char *)NULL);
+        }
+        else if(wybor == 3){
+            execlp("/home/student/PS_communicator/chat" , "chat" , (char *)NULL);
+        }
+        else if (wybor == 4)
+            return 0;
+        else{
+            printf("\nPodano nieepoprawna komende \n");
+            continue;
+        }
+    }
 }
