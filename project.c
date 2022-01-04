18ac4c4b08a1dceb8df0c962f290861217bf6e03
Michał Hasior <michalhasior@student.agh.edu.pl>
2022-01-04 11:50:36 +0100

    m

---
+++ project.c
@@ -0,0 +1,9 @@
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(){
+    _execl("/home/student/PS_communicator/client.exe" , "/home/student/PS_communicator/client.exe" , 4444, NULL );
+    return 0;
+}
