2d530edd0650d5d50ef690b3e3a6d9c4ba88fc51
hasiormichal <57234700+hasiormichal@users.noreply.github.com>
2022-01-20 19:04:01 +0100

    path

--- project.c
+++ project.c
@@ -13,11 +13,9 @@ int main(){
         printf("1) indywidualny czat - klient\n");
         printf("2) indywidualny czat - serwer\n");
         printf("3) czat grupowy\n");
-        //printf("4) czas\n");
-        //printf("5) czas_serwer\n");
         printf("4) wyjdz\n");
         scanf("%i",&wybor);
-
+        // w zależności od pobranej wartości program uruchomi odpowiedni podporogram za pomocą podanej ścieżki
         if(wybor == 1){
             execlp("/home/student/PS_communicator/client" , "client" ,"4444", (char *)NULL);
         }
@@ -26,11 +24,9 @@ int main(){
             execlp("/home/student/PS_communicator/server" , "server" ,"4444", (char *)NULL);
         }
         else if(wybor == 3){
-            execlp("/home/student/PS_communicator/chat" , "chat" , (char *)NULL);
+            //execlp("/home/student/PS_communicator/chat" , "chat" , (char *)NULL);
+            execlp("chat" , "chat" , (char *)NULL);
         }
-       // else if(wybor == 4){
-        //    execlp("/home/student/PS_communicator/czas" , "czas" , (char *)NULL);
-        //}
         else if (wybor == 4)
             return 0;
         else{
